7cdf282ad5770e4418fbe170971e21ca52c95545
Logan King <65763483+loganator956@users.noreply.github.com>
2023-01-22 22:15:11 +0000

    Add documentation

--- Assets/Character Systems/Scripts/ThirdPersonCamera3D.cs
+++ Assets/Character Systems/Scripts/ThirdPersonCamera3D.cs
@@ -7,13 +7,34 @@ namespace CharacterSystems.Movement
     public class ThirdPersonCamera3D : MonoBehaviour
     {
         [Header("Camera Movement Properties")]
+        /// <summary>
+        /// Controls the sensitivity on the horizontal (Y axis of rotation) axis
+        /// </summary>
         public float CameraHorizontalSensitivity = 20f;
+        /// <summary>
+        /// Controls the sensitivity on the vertical (X axis of rotation) axis
+        /// </summary>
         public float CameraVerticalSensitivity = 20f;
+        /// <summary>
+        /// Minimum X (Vertical) rotation
+        /// </summary>
         public float MinXRotation = -5f;
+        /// <summary>
+        /// Maximum X (Vertical) rotation
+        /// </summary>
         public float MaxXRotation = 90f;
+        /// <summary>
+        /// A curve that can be used to manipulate the distance of camera to player (VALUE) according to the X (Vertical) axis rotation (TIME). VALUE is the *multiplier* of distance
+        /// </summary>
         public AnimationCurve XRotationToDistanceCurve = new AnimationCurve(new Keyframe(-5f, 0.5f), new Keyframe(35f, 0.9f, 0.005f, 0.005f), new Keyframe(90f, 1f));
+        /// <summary>
+        /// The normal distance of camera to the player
+        /// </summary>
         public float CameraStandardDistance = 5f;
         [Header("Component References")]
+        /// <summary>
+        /// A reference to the camera transform. Doesn't *need* to be set as it will automatically select the main camera of the scene
+        /// </summary>
         public Transform CameraTransform;
 
         private Vector2 _inputs;
