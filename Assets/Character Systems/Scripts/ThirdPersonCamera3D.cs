c097989ea88f015e27af9db393be6ae6871a9238
Logan King <65763483+loganator956@users.noreply.github.com>
2023-01-23 17:45:46 +0000

    Create simple zoom

--- Assets/Character Systems/Scripts/ThirdPersonCamera3D.cs
+++ Assets/Character Systems/Scripts/ThirdPersonCamera3D.cs
@@ -20,6 +20,10 @@ namespace CharacterSystems.Movement
         /// </summary>
         public float CameraVerticalSensitivity = 20f;
         /// <summary>
+        /// Controls the sensitivity of zooming
+        /// </summary>
+        public float CameraZoomSensitivity = 1f;
+        /// <summary>
         /// Minimum X (Vertical) rotation
         /// </summary>
         public float MinXRotation = -5f;
@@ -32,9 +36,14 @@ namespace CharacterSystems.Movement
         /// </summary>
         public AnimationCurve XRotationToDistanceCurve = new AnimationCurve(new Keyframe(-5f, 0.5f), new Keyframe(10f, 0.9f, 0.005f, 0.005f), new Keyframe(90f, 1f));
         /// <summary>
-        /// The normal distance of camera to the player
+        /// The minimum distance of camera to the player
         /// </summary>
-        public float CameraStandardDistance = 5f;
+        public float CameraMinDistance = 4f;
+        /// <summary>
+        /// The maximum distance of camera to the player
+        /// </summary>
+        public float CameraMaxDistance = 12f;
+
         [Header("Component References")]
         /// <summary>
         /// A reference to the camera transform. Doesn't *need* to be set as it will automatically select the main camera of the scene
@@ -42,12 +51,19 @@ namespace CharacterSystems.Movement
         public Transform CameraTransform;
 
         private Vector2 _inputs;
+        private float _scrollInputs;
+        private float _currentZoomDistance = 0f;
         private float _xAngle, _yAngle;
         private CharacterMovement3D _characterMovement3D;
 
         public void OnLook(InputValue value)
         {
             _inputs = value.Get<Vector2>();
+        }
+
+        public void OnZoom(InputValue value)
+        {
+            _scrollInputs = value.Get<float>();
         }
 
         private void Awake()
@@ -60,7 +76,8 @@ namespace CharacterSystems.Movement
             {
                 Debug.LogWarning("Camera transform is not set, auto-detecting");
                 CameraTransform = Camera.main.transform;
-        }}
+            }
+        }
 
         private void Update()
         {
@@ -68,13 +85,17 @@ namespace CharacterSystems.Movement
             _yAngle += _inputs.x * CameraHorizontalSensitivity * Time.deltaTime;
             _xAngle += _inputs.y * CameraVerticalSensitivity * Time.deltaTime;
 
+            _currentZoomDistance -= _scrollInputs * Time.deltaTime;
+
             // clamp
             if (_yAngle > 360) { _yAngle -= 360; } else if (_yAngle < 0) { _yAngle += 360; };
             _xAngle = Mathf.Clamp(_xAngle, MinXRotation, MaxXRotation);
 
+            _currentZoomDistance = Mathf.Clamp(_currentZoomDistance, CameraMinDistance, CameraMaxDistance);
+
             // apply
             CameraTransform.localRotation = Quaternion.Euler(_xAngle, _yAngle, 0f);
-            CameraTransform.position = transform.position + CameraOffset + CameraTransform.forward * -1 * CameraStandardDistance * XRotationToDistanceCurve.Evaluate(_xAngle);
+            CameraTransform.position = transform.position + CameraOffset + CameraTransform.forward * -1 * _currentZoomDistance * XRotationToDistanceCurve.Evaluate(_xAngle);
             if (_characterMovement3D != null)
             {
                 Vector3 fwd = CameraTransform.forward;
